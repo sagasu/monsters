4986edd115ca293d26bf42e5ad46b29c9f0fe47f
Mateusz Kopij <sagasu@gmail.com>
2020-05-08 13:01:07 +0100

    Introducing card list component

--- monsters-play/src/App.js
+++ monsters-play/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { CardList } from './components/card-list/card-list.component';
 
 class App extends Component {
   constructor() {
@@ -31,6 +32,7 @@ class App extends Component {
   render() {
     return (
       <div className='App'>
+      <CardList name="foo"/>
         { 
           this.state.monsters.map(monster => <h1 key={monster.id}>{monster.name}</h1>) 
         }
