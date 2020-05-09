19885a1f4d0267b211ebf528f19bd991c77f06ec
Mateusz Kopij <sagasu@gmail.com>
2020-05-09 14:06:51 +0100

    Updates

---
+++ precache-manifest.ba73606cf8f10d42dc5ddcdefa0f67b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "251a34ae6d82405e1682ac83e299b54c",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "c53fd226b2e93707393c",
+    "url": "/monsters/static/css/main.4e06f39f.chunk.css"
+  },
+  {
+    "revision": "7ef5732205254c2ab3b0",
+    "url": "/monsters/static/js/2.8b5c5739.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters/static/js/2.8b5c5739.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c53fd226b2e93707393c",
+    "url": "/monsters/static/js/main.6f4eb760.chunk.js"
+  },
+  {
+    "revision": "ade3fb13bac37cc487c1",
+    "url": "/monsters/static/js/runtime-main.f749c510.js"
+  }
+]);
