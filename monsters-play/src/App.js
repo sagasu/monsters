178edcdc2250cd33b27a9b6c1243a863a1d571b5
Mateusz Kopij <sagasu@gmail.com>
2020-05-09 12:28:42 +0100

    extracting search logic to a seperate component.

--- monsters-play/src/App.js
+++ monsters-play/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { CardList } from './components/card-list/card-list.component';
+import {SearchBox} from './components/search-box/search-box.component';
 
 class App extends Component {
   constructor() {
@@ -25,11 +25,8 @@ class App extends Component {
 
     return (
       <div className='App'>
-        <input type="search" placeholder="search monsters" onChange={e => this.setState({searchField : e.target.value})} ></input>
-      <CardList monsters={filteredMonsters}>
-        
-      </CardList>
-        
+        <SearchBox placeholder="Search Monsters" handleChange={(e) => this.setState({searchField : e.target.value})}></SearchBox>
+        <CardList monsters={filteredMonsters} />        
       </div>
     );
   }
